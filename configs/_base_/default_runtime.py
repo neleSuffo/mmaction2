7d8cd7a870ade730870ae44e5505eb240dd30cef
NeleSuffo <nele.suffo@leuphana.de>
2024-11-05 07:05:21 +0100

    Enhance default_runtime.py with detailed comments for better clarity on configuration settings

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,24 +1,37 @@
-default_scope = 'mmaction'
+default_scope = 'mmaction'  # The default registry scope to find modules. Refer to https://mmengine.readthedocs.io/en/latest/tutorials/registry.html
 
-default_hooks = dict(
-    runtime_info=dict(type='RuntimeInfoHook'),
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=20, ignore_last=False),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1, save_best='auto'),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    sync_buffers=dict(type='SyncBuffersHook'))
+default_hooks = dict(  # Hooks to execute default actions like updating model parameters and saving checkpoints.
+    runtime_info=dict(type='RuntimeInfoHook'),  # The hook to updates runtime information into message hub
+    timer=dict(type='IterTimerHook'),  # The logger used to record time spent during iteration
+    logger=dict(
+        type='LoggerHook',  # The logger used to record logs during training/validation/testing phase
+        interval=20,  # Interval to print the log
+        ignore_last=False), # Ignore the log of last iterations in each epoch
+    param_scheduler=dict(type='ParamSchedulerHook'),  # The hook to update some hyper-parameters in optimizer
+    checkpoint=dict(
+        type='CheckpointHook',  # The hook to save checkpoints periodically
+        interval=1,  # The saving period
+        save_best='auto',  # Specified metric to mearsure the best checkpoint during evaluation
+        max_keep_ckpts=3),  # The maximum checkpoints to keep
+    sampler_seed=dict(type='DistSamplerSeedHook'),  # Data-loading sampler for distributed training
+    sync_buffers=dict(type='SyncBuffersHook'))  # Synchronize model buffers at the end of each epoch
 
-env_cfg = dict(
-    cudnn_benchmark=False,
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-    dist_cfg=dict(backend='nccl'))
+env_cfg = dict(  # Dict for setting environment
+    cudnn_benchmark=False,  # Whether to enable cudnn benchmark
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0), # Parameters to setup multiprocessing
+    dist_cfg=dict(backend='nccl')) # Parameters to setup distributed environment, the port can also be set
 
-log_processor = dict(type='LogProcessor', window_size=20, by_epoch=True)
+log_processor = dict(
+    type='LogProcessor',  # Log processor used to format log information
+    window_size=20,  # Default smooth interval
+    by_epoch=True)  # Whether to format logs with epoch type
 
-vis_backends = [dict(type='LocalVisBackend')]
-visualizer = dict(type='ActionVisualizer', vis_backends=vis_backends)
+vis_backends = [  # List of visualization backends
+    dict(type='LocalVisBackend')]  # Local visualization backend
+visualizer = dict(  # Config of visualizer
+    type='ActionVisualizer',  # Name of visualizer
+    vis_backends=vis_backends)
 
-log_level = 'INFO'
-load_from = None
-resume = False
+log_level = 'INFO'  # The level of logging
+load_from = None  # Load model checkpoint as a pre-trained model from a given path. This will not resume training.
+resume = False  # Whether to resume from the checkpoint defined in `load_from`. If `load_from` is None, it will resume the latest checkpoint in the `work_dir`.
