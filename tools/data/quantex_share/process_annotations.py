e93d72aaedc1ff72558731fa224fd473cfbf847a
NeleSuffo <nele.suffo@leuphana.de>
2024-12-12 13:48:38 +0100

    Add filtering for Quantex share annotations based on specified labels for bmn

--- tools/data/quantex_share/process_annotations.py
+++ tools/data/quantex_share/process_annotations.py
@@ -2,10 +2,15 @@
 """This file processes the annotation files and generates proper annotation
 files for localizers."""
 import json
-
 import numpy as np
 
-
+# labels for the boundary matching network (video only)
+include_labels = ["Watching something",
+                  "Drawing",
+                  "Crafting things",
+                  "Dancing",
+                  "Playing with Object"]
+    
 def load_json(file):
     with open(file) as json_file:
         data = json.load(json_file)
@@ -18,6 +23,25 @@ ann_file = f'{data_file}/quantex_share_annotations_mmaction2.json'
 
 anno_database = load_json(ann_file)
 
+# Filter the annotations
+filtered_annotations = {}
+for video_id, video_data in anno_database.items():
+    filtered_annotations[video_id] = {
+        "duration_second": video_data["duration_second"],
+        "duration_frame": video_data["duration_frame"],
+        "annotations": [
+            annotation for annotation in video_data["annotations"]
+            if annotation["label"] in include_labels
+        ]
+    }
+
+# Write the filtered annotations to a new JSON file
+with open(f'{data_file}/filtered_quantex_share_annotations_mmaction2.json', 'w') as f:
+    json.dump(filtered_annotations, f, indent=4)
+
+ann_file_filtered = f'{data_file}/filtered_quantex_share_annotations_mmaction2.json'
+anno_database_filtered = load_json(ann_file_filtered)
+
 video_record = np.loadtxt(info_file, dtype=str, delimiter=',', skiprows=1)
 
 video_dict_train = {}
@@ -27,7 +51,7 @@ video_dict_full = {}
 
 for _, video_item in enumerate(video_record):
     video_name = video_item[0]
-    video_info = anno_database[video_name]
+    video_info = anno_database_filtered[video_name]
     video_subset = video_item[5]
     video_info['fps'] = video_item[3].astype(np.float64)
     video_info['rfps'] = video_item[4].astype(np.float64)
