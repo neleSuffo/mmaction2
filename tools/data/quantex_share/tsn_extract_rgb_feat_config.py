05ac6796cd4e73cc981e19354bb1452d75be1600
NeleSuffo <nele.suffo@leuphana.de>
2024-12-14 21:59:30 +0100

    Update TSN configuration to use VideoDataset and adjust test pipeline settings

--- tools/data/quantex_share/tsn_extract_rgb_feat_config.py
+++ tools/data/quantex_share/tsn_extract_rgb_feat_config.py
@@ -2,18 +2,18 @@
 _base_ = [
     'mmaction::_base_/models/tsn_r50.py', 'mmaction::_base_/default_runtime.py'
 ]
-model = dict(cls_head=dict(num_classes=174, dropout_ratio=0.5))
 
 # dataset settings
-dataset_type = 'RawframeDataset'
+dataset_type = 'VideoDataset'
 data_root_val = 'data/kinetics400/rawframes_val'
 ann_file_val = 'data/kinetics400/kinetics400_val_list_rawframes.txt'
 
 file_client_args = dict(io_backend='disk')
 
 test_pipeline = [
-    dict(type='UntrimmedSampleFrames', clip_len=1, clip_interval=16),
-    dict(type='RawFrameDecode', **file_client_args),
+    dict(type='DecordInit', **file_client_args),
+    dict(type='SampleFrames', clip_len=8, frame_interval=2, num_clips=4),  # Adjust clip_len and num_clips
+    dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=256),
     dict(type='FormatShape', input_format='NCHW'),
@@ -21,7 +21,7 @@ test_pipeline = [
 ]
 
 test_dataloader = dict(
-    batch_size=12,
+    batch_size=6,
     num_workers=48,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
