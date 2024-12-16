cf1ea4eba767c6daabc24b0a20dbcbe851d3bcfe
NeleSuffo <nele.suffo@leuphana.de>
2024-12-16 21:50:35 +0100

    Adjust test dataloader configuration: reduce batch size to 1 and num_workers to 4

--- tools/data/quantex_share/tsn_extract_flow_feat_config.py
+++ tools/data/quantex_share/tsn_extract_flow_feat_config.py
@@ -25,8 +25,8 @@ test_pipeline = [
 ]
 
 test_dataloader = dict(
-    batch_size=6,
-    num_workers=48,
+    batch_size=1,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
