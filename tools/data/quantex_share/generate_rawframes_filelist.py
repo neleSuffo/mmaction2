40a76a8a49e30f9f889e3de0fe7682fa0514e051
NeleSuffo <nele.suffo@leuphana.de>
2024-12-16 21:49:56 +0100

    Update rawframes filelist generation for ChildLens dataset: modify paths and adjust label handling

--- tools/data/quantex_share/generate_rawframes_filelist.py
+++ tools/data/quantex_share/generate_rawframes_filelist.py
@@ -1,26 +1,36 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 import json
 import os
 import os.path as osp
 import cv2
+import csv
 
-data_file = '../../../data/quantex_share'
-video_list = f'{data_file}/video_info_new.csv' # 
-rawframe_dir = f'{data_file}/rawframes' # extracted rawframes
-action_name_list = 'action_name.csv'
-video_dir = f'{data_file}/videos'
+annotations_dir = '/home/nele_pauline_suffo/ProcessedData/annotations_superannotate'
+json_file = f'{annotations_dir}/childlens_annotations.json'  # annotation file
+video_info_file = f'{annotations_dir}/video_info.csv'  # video info file
+action_name_list = f'{annotations_dir}/action_name.csv'
+
+data_file = '/home/nele_pauline_suffo/ProcessedData/bmn_preprocessing'  # output directory
+rawframe_dir = f'{data_file}/rawframes'  # extracted rawframes
+video_dir = '/home/nele_pauline_suffo/ProcessedData/videos_superannotate_all'
 
 train_rawframe_dir = rawframe_dir
 val_rawframe_dir = rawframe_dir
 
-json_file = f'{data_file}/quantex_share.json' #annotation file
-
+def load_video_info(video_info_file):
+    video_info = {}
+    with open(video_info_file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            video_id = row['video'].replace('v_', '')
+            video_info[video_id] = row
+    return video_info
 
 def generate_rawframes_filelist():
-    database = json.load(open(json_file))
+    annotations = json.load(open(json_file))
+    video_info = load_video_info(video_info_file)
 
-    quantex_labels = open(action_name_list).readlines()
-    quantex_labels = [x.strip() for x in quantex_labels[1:]]
+    childlens_labels = open(action_name_list).readlines()
+    childlens_labels = [x.strip() for x in childlens_labels[1:]]
 
     train_dir_list = [
         osp.join(train_rawframe_dir, x) for x in os.listdir(train_rawframe_dir)
@@ -31,7 +41,7 @@ def generate_rawframes_filelist():
 
     def simple_label(anno):
         label = anno[0]['label']
-        return quantex_labels.index(label)
+        return childlens_labels.index(label)
     
     def count_frames(dir_list, video):
         for dir_name in dir_list:
@@ -48,13 +58,13 @@ def generate_rawframes_filelist():
     validation = {}
     key_dict = {}
 
-    for k in database:
-        data = database[k]
-        subset = data['subset']
+    for video_id in annotations:
+        data = annotations[video_id]
+        subset = video_info[video_id]['subset']
 
         if subset in ['training', 'validation']:
-            annotations = data['annotations']
-            label = simple_label(annotations)
+            video_annotations = data['annotations']
+            label = simple_label(video_annotations)
             if subset == 'training':
                 dir_list = train_dir_list
                 data_dict = training
@@ -62,14 +72,11 @@ def generate_rawframes_filelist():
                 dir_list = val_dir_list
                 data_dict = validation
 
-        else:
-            continue
-
-        gt_dir_name, num_frames = count_frames(dir_list, k)
-        if gt_dir_name is None:
-            continue
-        data_dict[gt_dir_name] = [num_frames, label]
-        key_dict[gt_dir_name] = k
+            gt_dir_name, num_frames = count_frames(dir_list, video_id)
+            if gt_dir_name is None:
+                continue
+            data_dict[gt_dir_name] = [num_frames, label]
+            key_dict[gt_dir_name] = video_id
 
     train_lines = [
         k + ' ' + str(training[k][0]) + ' ' + str(training[k][1])
@@ -80,9 +87,9 @@ def generate_rawframes_filelist():
         for k in validation
     ]
 
-    with open(osp.join(data_file, 'quantex_share_train_video.txt'), 'w') as fout:
+    with open(osp.join(data_file, 'childlens_train_video.txt'), 'w') as fout:
         fout.write('\n'.join(train_lines))
-    with open(osp.join(data_file, 'quantex_share_val_video.txt'), 'w') as fout:
+    with open(osp.join(data_file, 'childlens_val_video.txt'), 'w') as fout:
         fout.write('\n'.join(val_lines))
 
     def get_video_info(video_path):
@@ -102,7 +109,7 @@ def generate_rawframes_filelist():
         for annotation in annotations:
             segment = annotation['segment']
             label = annotation['label']
-            label = quantex_labels.index(label)
+            label = childlens_labels.index(label)
             start, end = int(segment[0] * fps), int(segment[1] * fps)
             duration = end - start + 1
             if end > num_frames - 1:
@@ -117,15 +124,14 @@ def generate_rawframes_filelist():
 
     train_clips, val_clips = [], []
     for k in training:
-        train_clips.extend(clip_list(k, database[key_dict[k]]))
+        train_clips.extend(clip_list(k, annotations[key_dict[k]]))
     for k in validation:
-        val_clips.extend(clip_list(k, database[key_dict[k]]))
+        val_clips.extend(clip_list(k, annotations[key_dict[k]]))
 
-    with open(osp.join(data_file, 'quantex_share_train_clip.txt'), 'w') as fout:
+    with open(osp.join(data_file, 'childlens_train_clip.txt'), 'w') as fout:
         fout.write('\n'.join(train_clips))
-    with open(osp.join(data_file, 'quantex_share_val_clip.txt'), 'w') as fout:
+    with open(osp.join(data_file, 'childlens_val_clip.txt'), 'w') as fout:
         fout.write('\n'.join(val_clips))
-
 
 if __name__ == '__main__':
     generate_rawframes_filelist()
