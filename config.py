782f70c4bef24fd760c3d80c2dc3172624c1513d
NeleSuffo <nele.suffo@leuphana.de>
2024-12-18 14:18:50 +0100

    Refactor config.py to improve path management and rename ActivityLocalization to AnnotationProcessing

--- config.py
+++ config.py
@@ -14,26 +14,29 @@ settings = Dynaconf(
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+data_dir = Path("/home/nele_pauline_suffo/ProcessedData")
 
-class ActivityLocalization:
+class AnnotationProcessing:
     # directory with individual json annotations files
-    annotations_dir = Path("/home/nele_pauline_suffo/ProcessedData/annotations_superannotate")
+    annotations_dir = Path(f"{data_dir}/annotations_superannotate")
     # Path to file that contains all annotations combined (generated from generate_combined_annotations.py)
-    combined_annotation_path = Path(f'{annotations_dir}/childlens_annotations.json')
+    combined_annotation_path = Path(f"{annotations_dir}/childlens_annotations.json")
     # List of labels to include in combined json file in "projects/mmaction2/tools/data/quantex_share/generate_combined_annotations.py"
     activities_to_include = [
     "Playing with Object",
     ]
+    
+class VideoProcessing:
     # train ratio for splitting videos into training and validation
     childlens_train_ratio: float = 0.8
     # Path to file that contains video information like number of frames, training and testing assignment(generated from generate_video_info.py)
-    video_info_path = Path(f'{annotations_dir}/video_info.csv')
-    bmn_preprocessing_dir = '/home/nele_pauline_suffo/ProcessedData/bmn_preprocessing'
+    video_info_path = Path(f"{AnnotationProcessing.annotations_dir}/video_info.csv")
+    bmn_preprocessing_dir = Path(f"{data_dir}/bmn_preprocessing")
 
 class FrameExtraction:
     #parameters needed when running extract_frames.py
-    video_input_dir = "/home/nele_pauline_suffo/ProcessedData/videos_superannotate_all"
-    rawframes_output_dir = "/home/nele_pauline_suffo/ProcessedData/videos_superannotate/rawframes"
+    video_input_dir = Path(f"{data_dir}/videos_superannotate_all")
+    rawframes_output_dir = Path(f"{data_dir}/videos_superannotate/rawframes")
     # whether video files are stored as .mp4 or .MP4
     video_ext = "MP4"
     # extract only "rgb", "flow" or "both"
@@ -43,16 +46,4 @@ class FrameExtraction:
     level = 1
     flow_type = "tvl1"
     resume = True
-
-
-
-rawframe_dir = "/home/nele_pauline_suffo/ProcessedData/bmn_preprocessing/rawframes"
-video_dir = "/home/nele_pauline_suffo/ProcessedData/videos_superannotate_all"
-action_name_list = "/home/nele_pauline_suffo/ProcessedData/annotations_superannotate/action_name.csv"
-json_file = "/home/nele_pauline_suffo/ProcessedData/annotations_superannotate/childlens_annotations.json"
-video_info_file = "/home/nele_pauline_suffo/ProcessedData/annotations_superannotate/video_info.csv"
-
-
-
-
-
+    denseflow_installation_path = "/home/nele_pauline_suffo/app/bin/denseflow"
