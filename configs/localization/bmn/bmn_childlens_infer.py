c04d6b4bdf8d346b70cb3935974d29028b994713
NeleSuffo <nele.suffo@leuphana.de>
2025-02-06 15:00:36 +0100

    Add inference configuration for childlens video processing

---
+++ configs/localization/bmn/bmn_childlens_infer.py
@@ -0,0 +1,37 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+_base_ = ['../../_base_/models/bmn_400x100.py']
+
+# Modify the model configuration
+model = dict(
+    cls_head=dict(average_clips='prob')
+)
+
+# dataset settings
+dataset_type = 'VideoDataset'
+test_pipeline = [
+    dict(type='DecordInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=1,
+        frame_interval=1,
+        num_clips=8,
+        test_mode=True),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='PackActionInputs')
+]
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        ann_file=None,
+        data_prefix=None,
+        pipeline=test_pipeline))
+
+
+
+
