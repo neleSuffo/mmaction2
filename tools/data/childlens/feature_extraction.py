71419290f4d9d695a5500ca678ffe7978a7d651f
NeleSuffo <nele.suffo@leuphana.de>
2025-07-24 00:46:36 +0200

    Uncomment train video list in feature extraction commands

--- tools/data/childlens/feature_extraction.py
+++ tools/data/childlens/feature_extraction.py
@@ -6,8 +6,8 @@ def generate_rgb_command():
     command = (
         f"python ../../misc/clip_feature_extraction.py {config.FeatureExtraction.rgb_config_file} "
         f"{config.FeatureExtraction.pretrained_model_url} {config.FeatureExtraction.rgb_output_dir} "
-        #f"--video-list {config.FrameExtraction.train_video_txt_path} "
-        f"--video-list {config.FrameExtraction.val_video_txt_path} "
+        f"--video-list {config.FrameExtraction.train_video_txt_path} "
+        #f"--video-list {config.FrameExtraction.val_video_txt_path} "
         f"--video-root {config.FrameExtraction.rawframes_processed_dir} "
     )
     return command
@@ -17,8 +17,8 @@ def generate_flow_command():
     command = (
         f"python ../../misc/clip_feature_extraction.py {config.FeatureExtraction.flow_config_file} "
         f"{config.FeatureExtraction.pretrained_model_url} {config.FeatureExtraction.flow_output_dir} "
-        #f"--video-list {config.FrameExtraction.train_video_txt_path} "
-        f"--video-list {config.FrameExtraction.val_video_txt_path} "
+        f"--video-list {config.FrameExtraction.train_video_txt_path} "
+        #f"--video-list {config.FrameExtraction.val_video_txt_path} "
         f"--video-root {config.FrameExtraction.rawframes_processed_dir} "
     )
     return command
